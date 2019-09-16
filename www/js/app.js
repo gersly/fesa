7b09286f17c1185caa6d94ef94da9189518c8211
debesteste <gersonlynch@gmail.com>
2019-09-16 21:04:57 +0200

    initial commit

---
+++ www/js/app.js
@@ -0,0 +1,38 @@
+var $$ = Dom7;
+
+var app = new Framework7({
+  root: '#app', // App root element
+
+  name: 'Tickits', // App name
+  theme: 'auto', // Automatic theme detection
+  // App root data
+  data: function () {
+    return {
+      user: {
+        firstName: 'John',
+        lastName: 'Doe',
+      },
+
+    };
+  },
+  // App root methods
+  methods: {
+    helloWorld: function () {
+      app.dialog.alert('Hello World!');
+    },
+  },
+  // App routes
+  routes: routes,
+});
+
+// create searchbar
+var searchbar = app.searchbar.create({
+  el: '.searchbar',
+  searchContainer: '.list',
+  searchIn: '.item-title',
+  on: {
+    search(sb, query, previousQuery) {
+      console.log(query, previousQuery);
+    }
+  }
+});
