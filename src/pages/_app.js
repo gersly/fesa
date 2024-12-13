231ef3d30e246b3751d2d690bcccc10054ac40d8
gersly <gersonlynch@gmail.com>
2024-12-13 04:15:46 -0300

    lintfix

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -33,31 +33,8 @@ export default function App(
   return (
     <>
       <Head>
-        {/*<Script>
-          !function(f,b,e,v,n,t,s)
-          {if(f.fbq)return;n=f.fbq=function(){n.callMethod ?
-            n.callMethod.apply(n, arguments) : n.queue.push(arguments)};
-          if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-          n.queue=[];t=b.createElement(e);t.async=!0;
-          t.src=v;s=b.getElementsByTagName(e)[0];
-s.parentNode.insertBefore(t,s)}(window, document,'script',
-          'https://connect.facebook.net/en_US/fbevents.js');
-          fbq('init', '255779914831607');
-          fbq('track', 'PageView');
-        </Script>*/}
-        <Script
-          dangerouslySetInnerHTML={{
-            __html: ` !function(f,b,e,v,n,t,s)
-          {if(f.fbq)return;n=f.fbq=function(){n.callMethod ?
-            n.callMethod.apply(n, arguments) : n.queue.push(arguments)};
-          if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-          n.queue=[];t=b.createElement(e);t.async=!0;
-          t.src=v;s=b.getElementsByTagName(e)[0];
-s.parentNode.insertBefore(t,s)}(window, document,'script',
-          'https://connect.facebook.net/en_US/fbevents.js');
-          fbq('init', '255779914831607');
-          fbq('track', 'PageView');`
-          }} />
+
+
         <noscript><img height="1" width="1" style="display:none"
           src="https://www.facebook.com/tr?id=255779914831607&ev=PageView&noscript=1"
         /></noscript>
@@ -135,6 +112,20 @@ s.parentNode.insertBefore(t,s)}(window, document,'script',
       <PostHogProvider client={posthog}>
         <MapProvider>
           <Component {...pageProps} />
+          <Script
+            id='facebook-pixel'
+            dangerouslySetInnerHTML={{
+              __html: ` !function(f,b,e,v,n,t,s)
+          {if(f.fbq)return;n=f.fbq=function(){n.callMethod ?
+            n.callMethod.apply(n, arguments) : n.queue.push(arguments)};
+          if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+          n.queue=[];t=b.createElement(e);t.async=!0;
+          t.src=v;s=b.getElementsByTagName(e)[0];
+s.parentNode.insertBefore(t,s)}(window, document,'script',
+          'https://connect.facebook.net/en_US/fbevents.js');
+          fbq('init', '255779914831607');
+          fbq('track', 'PageView');`
+            }} />
         </MapProvider>
         <Analytics />
 
